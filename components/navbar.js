4b81666da7c83676e362352101442edbe4124408
Rabiyanaz <rabiyanaz1213@gmail.com>
2022-07-20 21:02:02 +0530

    project-2

---
+++ components/navbar.js
@@ -0,0 +1,53 @@
+ 
+ 
+let navbar =()=>{
+return `
+<div id="nav1">
+<div ></div>
+<div><h1>myfitnesspal</h1></div>
+<div></div>
+<div>
+    <div> <a href="login.html">Login</a></div>
+    <div>|</div>
+    <div>Sign Up</div>
+</div>
+<div></div>
+</div>
+
+<!-- Start navbar -->
+
+
+<div class="navBar">
+    <a href="About.html">
+        <div class="menu">ABOUT</div>
+    </a>
+    <a href="food.html">
+        <div class="menu">FOOD</div>
+    </a>
+    <a href="exercise.html">
+        <div class="menu">EXCERCISE</div>
+    </a>
+    <a href="Apps.html">
+        <div class="menu">APPS</div>
+    </a>
+    <a href="community.html">
+        <div class="menu">COMMUNITY</div>
+    </a>
+    <a href="Blog.html">
+        <div class="menu">BLOG</div>
+    </a>
+    <a href="Premium.html">
+        <div class="menu">PREMIUM</div>
+    </a>
+  </div>
+
+  <div id="main">
+    <div id="fitness">
+        <div></div>
+        <div></div>
+        <div></div>
+    </div> 
+    </div>
+    `
+ }
+ export default navbar
---
+++ index.js
@@ -0,0 +1,3 @@
+import navbar from "./components/navbar.js"
+// console.log(navbar())
+document.getElementById("nav100").innerHTML=navbar()
