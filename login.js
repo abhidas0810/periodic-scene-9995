b09d65e5fc070b2b425bf96a5e4f84a597dc107c
dhullsuman <103638627+dhullsuman@users.noreply.github.com>
2022-07-20 19:37:57 +0530

    project-2

---
+++ login.js
@@ -0,0 +1,31 @@
+let data=JSON.parse(localStorage.getItem("users")) ||[];
+
+
+document.querySelector("form").addEventListener("submit",allData)
+
+function allData(evemt){
+    event.preventDefault();
+    let email=document.getElementById("email").value;
+    let password=document.getElementById("password").value;
+
+
+if(password=="") alert("Wrong credentials")
+else if(email==""){
+    alert("User doesn't exist, Sign Up")
+}else{
+       if(mailCheck(email,password)==true){
+       alert("Login successful!")
+       window.location.href="index.html"
+    }else{
+        alert("Alredy Have an Account")
+    }
+   
+}
+}
+let mailCheck=(mail,pass)=>{
+    let f=data.filter((elem)=>{
+        return mail===elem.email && pass==elem.password;
+    });
+    if(f.length>0) return true;
+    else return false;
+}
