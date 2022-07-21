82af326336d617069f8588dbd88dabd51554212f
Abhishek Das <abhidas0810@gmail.com>
2022-07-21 20:49:22 +0530

    apps

--- apps.js
+++ apps.js
@@ -1,8 +1,8 @@
-// import navbar from "../components/navbar.js";
-// document.getElementById("navbar").innerHTML = navbar();
+import navbar from "../components/navbar.js";
+document.getElementById("navbar").innerHTML = navbar();
 
-// import { footer } from "./footer.js";
-// document.getElementById("footer").innerHTML = footer();
+import { footer } from "./footer.js";
+document.getElementById("footer").innerHTML = footer();
 
 // ----------------------------------------------------featured------------------------------
 
